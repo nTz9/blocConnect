45dcf37e0356d814ff8e5ed2dee668250bc5db8a
nTz9 <stefandanielpetru.nita@ulbsibiu.ro>
2023-11-23 01:37:50 +0200

    connect firebase to application

---
+++ blocConnect/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { environment } from 'src/environments/environment';
+
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ blocConnect/src/environments/environment.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "@angular/fire/app";
+import { getFirestore } from "@angular/fire/firestore";
+
+export const environment = {
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyAqFD3nTOCxPebFb9JTtQ9Bc5VwgxvvMkY",
+        authDomain: "blocconnect-b8643.firebaseapp.com",
+        projectId: "blocconnect-b8643",
+        storageBucket: "blocconnect-b8643.appspot.com",
+        messagingSenderId: "585540162473",
+        appId: "1:585540162473:web:6815e74ed95a9c089beab8",
+        measurementId: "G-SPDRJPJQJL"
+      }
+  };
