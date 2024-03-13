7b37f1309e421394d94e47fb253d3174f8cbadab
nTz9 <stefandanielpetru.nita@ulbsibiu.ro>
2024-03-13 18:46:04 +0200

    Implement Water Meter List Table

--- blocConnect/src/app/components/water-meter/water-meter-list/water-meter-list/water-meter-list.component.ts
+++ blocConnect/src/app/components/water-meter/water-meter-list/water-meter-list/water-meter-list.component.ts
@@ -12,6 +12,11 @@ import { WaterMeterService } from 'src/app/services/water-meter.service';
 })
 export class WaterMeterListComponent {
 
+  meterReadings: any = [];
+  apartaments: any = [];
+
+  currentPage = 1;
+  itemsPerPage = 2;
 
   ngOnInit() {
     this.getMeterReadings();
@@ -31,9 +36,39 @@ export class WaterMeterListComponent {
     this.userService.getLoggedUserId().subscribe(cnp => {
       this.waterMeterService.getMeterReadingsByCNP(cnp).subscribe(data => {
         if(data){
-          console.log(data);
+          this.meterReadings = data.sort((a,b) => new Date(a.readingDate).getTime() - new Date(b.readingDate).getTime());
         }
       });
     })
   }
+
+  // getMeterReadingsByApartament() {
+  //   this.userService.getLoggedUserId().subscribe(cnp => {
+  //     this.waterMeterService.getMeterReadingsByCNP(cnp).subscribe(data => {
+  //       if(data){
+  //         const apartamentNumber = [...new Set(data.map(reading => reading.apartamentNumber))];
+  //         this.apartaments = apartamentNumber.map(apartament => ({
+  //           apartamentNumber: apartament,
+  //           meterReadings: data.filter(reading => reading.apartamentNumber === apartament)
+  //         }));
+  //       }
+  //     });
+  //   })
+  // }
+
+  getPageCount(): number {
+    return Math.ceil(this.meterReadings.length / this.itemsPerPage);
+  }
+  getPaginatedData(): any[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.meterReadings.slice(startIndex, endIndex);
+  }
+
+  goToPage(pageNumber: number): void {
+    if(pageNumber>= 1 && pageNumber <= this.getPageCount()){
+      this.currentPage = pageNumber;
+    }
+  }
+  
 }
