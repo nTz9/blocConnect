cd2aaebb9d11c1bc95e2104f0512e8d1fd7829e0
nTz9 <stefandanielpetru.nita@ulbsibiu.ro>
2024-04-24 13:43:28 +0300

    Implement announcement for any block

--- blocConnect/src/app/components/announcement/announcement/announcement.component.ts
+++ blocConnect/src/app/components/announcement/announcement/announcement.component.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, from, map, switchMap } from 'rxjs';
+import { AnnouncementService } from 'src/app/services/announcement.service';
+import { ApartamentService } from 'src/app/services/apartament.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { BlockService } from 'src/app/services/block.service';
+import { UserService } from 'src/app/services/user.service';
+import { WaterMeterService } from 'src/app/services/water-meter.service';
 
 @Component({
   selector: 'app-announcement',
   templateUrl: './announcement.component.html',
   styleUrls: ['./announcement.component.css']
 })
-export class AnnouncementComponent {
+export class AnnouncementComponent implements OnInit{
   
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private firestore: AngularFirestore,
+    private userService: UserService,
+    private apartamentService: ApartamentService,
+    private waterMeterService: WaterMeterService,
+    private blockService: BlockService,
+    private announcementService: AnnouncementService
+  ) { }
 
   ngOnInit(): void {
+    this.getAnnouncement();
   }
+
+  announcements: any = [];
+  
+
+  user$ = this.authService.getCurrentUser();
+  getAnnouncement() {
+    this.userService.getLoggedUserId().subscribe(cnp => {
+      this.apartamentService.getAvailableApartamentsByCNP(cnp).subscribe(apartament => {
+        const blockIds = apartament.map((apartament: { blockID: any; }) => apartament.blockID);
+        console.log(blockIds);
+        this.announcementService.getAnnouncementsForBlocks(blockIds).subscribe(announcement => {
+          this.announcements = announcement;
+          console.log(this.announcements);
+        })
+      })
+    })
+  }
+
+  // getAnnouncement() {
+  //   this.userService.getLoggedUserId().pipe(
+  //     switchMap(cnp => this.apartamentService.getAvailableApartamentsByCNP(cnp)),
+  //     map(apartaments => apartaments.map(apartament => apartament.blockID)),
+  //     switchMap(blockIds => this.announcementService.getAnnouncementsForBlocks(blockIds))
+  //   ).subscribe(announcements => {
+  //     this.announcements = announcements;
+  //     console.log(this.announcements);
+  //   });
+  // }
+
+
 }
---
+++ blocConnect/src/app/services/announcement.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, from, map, mergeAll, mergeMap, switchMap, toArray } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnnouncementService {
+
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+
+  getAnnouncementsForBlocks(blockIds: string[]): Observable<any[]> {
+    console.log("blockIds received:", blockIds);
+    return from(blockIds).pipe(
+      mergeMap(blockId => {
+        console.log("Querying for blockId:", blockId);
+        return this.firestore.collection('announcement', ref => ref.where('blocId', '==', blockId)).valueChanges();
+      }),
+      //mergeAll(), // Combinați toate răspunsurile într-un singur flux de date
+      //toArray() // Grupați toate anunțurile într-un singur array
+    );
+  }
+
+}
